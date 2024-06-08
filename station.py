064728fcb954777fdce58f830392f8c7fd9ca18a
AmDyis <86226916+AmDyis@users.noreply.github.com>
2024-06-08 17:34:40 +0300

    Update station.py
    change password

--- station.py
+++ station.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:4545@localhost/anta"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:********@localhost/anta"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
